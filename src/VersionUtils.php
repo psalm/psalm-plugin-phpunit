206ca87c6692965e0def516ba0213c1cf1df9e8c
Bruce Weirdan <weirdan@gmail.com>
2020-08-24 21:37:29 +0300

    Fix DeprecatedMethod

---
+++ src/VersionUtils.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Psalm\PhpUnitPlugin;
+
+use Composer\Semver\Comparator;
+use Composer\Semver\VersionParser;
+use PackageVersions\Versions;
+
+abstract class VersionUtils
+{
+    public static function packageVersionIs(string $package, string $op, string $ref): bool
+    {
+        /**
+         * @psalm-suppress DeprecatedClass
+         * @psalm-suppress ArgumentTypeCoercion
+         */
+        $currentVersion = (string) explode('@', Versions::getVersion($package))[0];
+
+        $parser = new VersionParser();
+
+        $currentVersion = $parser->normalize($currentVersion);
+        $ref = $parser->normalize($ref);
+
+        return Comparator::compare($currentVersion, $op, $ref);
+    }
+}
