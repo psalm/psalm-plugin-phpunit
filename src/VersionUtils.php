9e3f0da9a2531578c9b0cb946907ef8e3d3e0e1a
Dawid 'DeyV' Polak <dawid.polak@mentax.pl>
2021-12-28 08:54:20 +0100

    Move from deprecated Composer 1 to Composer 2 runtime api

--- src/VersionUtils.php
+++ src/VersionUtils.php
@@ -2,32 +2,18 @@
 
 namespace Psalm\PhpUnitPlugin;
 
+use Composer\InstalledVersions;
 use Composer\Semver\Comparator;
-use Composer\Semver\VersionParser;
-use PackageVersions\Versions;
 
 abstract class VersionUtils
 {
     /** @param "!="|"<"|"<="|"<>"|"="|"=="|">"|">=" $op */
     public static function packageVersionIs(string $package, string $op, string $ref): bool
     {
-        try {
-            /**
-             * @psalm-suppress RedundantCondition
-             * @psalm-suppress DeprecatedClass
-             * @psalm-suppress ArgumentTypeCoercion
-             * @psalm-suppress RedundantCast
-             * @psalm-suppress RedundantCondition
-             */
-            $currentVersion = (string) explode('@', Versions::getVersion($package))[0];
-        } catch (\OutOfBoundsException $exception) {
+        $currentVersion = InstalledVersions::getVersion($package);
+        if (is_null($currentVersion)) {
             return false;
         }
-
-        $parser = new VersionParser();
-
-        $currentVersion = $parser->normalize($currentVersion);
-        $ref = $parser->normalize($ref);
 
         return Comparator::compare($currentVersion, $op, $ref);
     }
